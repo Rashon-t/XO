4a063c43157e1e02c01be2d0b56b1e20875ad665
fn-cafeina <jasmirp5@gmail.com>
2024-05-31 02:01:18 -0600

    code refactoring and added minimax algorithm

--- src/main.js
+++ src/main.js
@@ -12,11 +12,7 @@ const Game = {
     gameLocked: false,
     xScore: 0,
     oScore: 0,
-    board: [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0
-    ],
+    board: Array(9).fill(0),
     players: [
         { mark: "X" },
         { mark: "O" }
@@ -24,56 +20,70 @@ const Game = {
     isXTurn: true
 }
 
+const WINNING_PATTERNS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
 App.divs.forEach((div, index) => {
     div.addEventListener('click', () => {
         if (!Game.gameLocked) {
-            let isCellOccupied = div.textContent === "X" || div.textContent === "O";
-            if (!isCellOccupied) {
-                Game.isXTurn = draw(Game.isXTurn, div, index);
-                play(Game.board);
-            }
-            color();
+            handlePlayerMove(div, index);
         }
-    })
-})
+    });
+});
+
+App.restartButton.addEventListener('click', resetGame);
+
+function handlePlayerMove(div, index) {
+    if (div.textContent !== "") return;
+    Game.isXTurn = draw(Game.isXTurn, div, index);
+    play(Game.board);
+    if (!Game.isXTurn && !Game.gameLocked) {
+        Game.gameLocked = true;
+        setTimeout(() => {
+            botMove();
+            if (!checkWinner(Game.board) && Game.board.includes(0)) {
+                Game.gameLocked = false;
+            }
+        }, 700);
+    }
+    color();
+}
 
 function draw(isXTurn, div, index) {
-    let xMark = Game.players[0].mark;
-    let oMark = Game.players[1].mark;
-    if (isXTurn) {
-        div.textContent = xMark;
-        Game.board[index] = xMark;
-        return false;
-    } else {
-        div.textContent = oMark;
-        Game.board[index] = oMark;
-        return true;
-    }
+    const mark = isXTurn ? Game.players[0].mark : Game.players[1].mark;
+    div.textContent = mark;
+    Game.board[index] = mark;
+    return !isXTurn;
 }
 
 function play(board) {
-    const winningPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
-    for (let pattern of winningPatterns) {
+    const winner = checkWinner(board);
+    if (winner) {
+        highlightWinningCells(winner.pattern);
+        updateScore(winner.mark);
+        lockGame();
+    } else if (!board.includes(0)) {
+        App.divs.forEach(div => div.classList.add("winner"));
+        lockGame();
+    }
+}
+
+function checkWinner(board) {
+    for (let pattern of WINNING_PATTERNS) {
         const [a, b, c] = pattern;
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            const winningCells = [a, b, c];
-            for (let cellIndex of winningCells) {
-                App.divs[cellIndex].classList.add("winner");
-            }
-            updateScore(board[a]);
-            lockGame();
-            return;
+            return { mark: board[a], pattern };
         }
     }
-    if (!board.includes(0)) {
-        App.divs.forEach((i) => {
-            i.classList.add("winner");
-        })
-        lockGame();
+    return null;
+}
+
+function highlightWinningCells(pattern) {
+    for (let index of pattern) {
+        App.divs[index].classList.add("winner");
     }
 }
 
@@ -97,16 +107,8 @@ function updateScore(winnerMark) {
     }
 }
 
-App.restartButton.addEventListener('click', () => {
-    resetGame();
-});
-
 function resetGame() {
-    Game.board = [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0
-    ];
+    Game.board = Array(9).fill(0);
     Game.isXTurn = true;
     App.divs.forEach(div => {
         div.textContent = '';
@@ -122,4 +124,39 @@ function lockGame() {
 
 function unlockGame() {
     Game.gameLocked = false;
+}
+
+function botMove() {
+    let bestMove = minimax(Game.board, Game.players[1].mark).index;
+    let div = App.divs[bestMove];
+    Game.isXTurn = draw(Game.isXTurn, div, bestMove);
+    play(Game.board);
+    color();
+}
+
+function minimax(newBoard, player) {
+    const availSpots = newBoard.reduce((acc, val, index) => {
+        if (val === 0) acc.push(index);
+        return acc;
+    }, []);
+    const winner = checkWinner(newBoard);
+    if (winner) {
+        return { score: winner.mark === Game.players[1].mark ? 10 : -10 };
+    } else if (availSpots.length === 0) {
+        return { score: 0 };
+    }
+    const moves = availSpots.map(spot => {
+        const move = { index: spot };
+        newBoard[spot] = player;
+        move.score = minimax(newBoard, player === Game.players[1].mark ? Game.players[0].mark : Game.players[1].mark).score;
+        newBoard[spot] = 0;
+        return move;
+    });
+    return moves.reduce((best, move) => {
+        if ((player === Game.players[1].mark && move.score > best.score) ||
+            (player === Game.players[0].mark && move.score < best.score)) {
+            return move;
+        }
+        return best;
+    }, moves[0]);
 }
